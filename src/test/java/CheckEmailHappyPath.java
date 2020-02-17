fe9debf7fe23c6ae58f03383237ce6f310dbe8ec
Bohdan Sokhatskyi <sohatzky.bogdan@gmail.com>
2020-02-17 15:03:45 +0200

    HomeTask1 fixed

--- src/test/java/CheckEmailHappyPath.java
+++ src/test/java/CheckEmailHappyPath.java
@@ -1,13 +1,18 @@
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class CheckEmailHappyPath {
-    @Test(dataProvider = "Email", dataProviderClass = UiTestsCases.class)
-    public void checkHappyCase(String email) {
+    @DataProvider(name = "emailsForHappyPath")
+    public Object[][] dataProviderMethod() {
+        return new Object[][]{
+                {"soHAtzky13!_@KnU1.com"}
+        };
+    }
+
+    @Test(dataProvider = "emailsForHappyPath")
+    public void checkHappyPath(String email) {
         boolean actualResult = Email.isEmailCorrect(email);
-        Assert.assertEquals(actualResult, true);
+        Assert.assertTrue(true, "Email is incorrect");
     }
 }
--- src/test/java/CheckEmailUnhappyPath.java
+++ src/test/java/CheckEmailUnhappyPath.java
@@ -1,14 +1,34 @@
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
-import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class CheckEmailUnhappyPath {
-    @Test(dataProvider = "Email", dataProviderClass = UiTestsCases.class)
-    public void checkUnhappyCase(String email) {
+    @DataProvider(name = "emailsForUnhappyPath")
+    public Object[][] dataProviderMethod() {
+        return new Object[][]{
+                {"sohatzkygmail.com"},
+                {"sohatzky@gmailcom"},
+                {"so@gmail.com"},
+                {"soh@gmail.com"},
+                {"12345678901234567890@gmail.com"},
+                {"123456789012345678901@gmail.com"},
+                {"?????@gmail.com"},
+                {"sohatzky@g.com"},
+                {"sohatzky@gm.com"},
+                {"sohatzky@gmail.com"},
+                {"sohatzky@gmaill.com"},
+                {"sohatzky@???.com"},
+                {"sohatzky@gmail.c"},
+                {"sohatzky@gmail.co"},
+                {"sohatzky@g.commm"},
+                {"sohatzky@g.commmm"},
+                {"sohatzky@gmail.???"},
+                {"sohatzky@gmail.COM"}};
+    }
+
+    @Test(dataProvider = "emailsForUnhappyPath")
+    public void checkUnhappyPath(String email) {
         boolean actualResult = Email.isEmailCorrect(email);
-        Assert.assertEquals(actualResult, false);
+        Assert.assertFalse(false, "Email is expected to be incorrect, but it's correct");
     }
 }
