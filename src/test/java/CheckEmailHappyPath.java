930e7f7d0b0f60e3d39ccb07f88b077de325d064
Bohdan Sokhatskyi <sohatzky.bogdan@gmail.com>
2020-02-21 22:56:02 +0200

    HomeTask1 fixed 2.0

--- src/test/java/CheckEmailHappyPath.java
+++ src/test/java/CheckEmailHappyPath.java
@@ -8,10 +8,15 @@ public class CheckEmailHappyPath {
         return new Object[][]{
                 {"soHAtzky13!_@KnU1.com"},
                 {"soh@gmail.com"},
+                {"soha@gmail.com"},
+                {"1234567890123456789@gmail.com"},
                 {"12345678901234567890@gmail.com"},
                 {"sohatzky@gm.com"},
+                {"sohatzky@gma.com"},
+                {"sohatzky@gmai.com"},
                 {"sohatzky@gmail.com"},
                 {"sohatzky@gmail.co"},
+                {"sohatzky@gmail.comm"},
                 {"sohatzky@gmail.commm"}
         };
     }
--- src/test/java/CheckEmailUnhappyPath.java
+++ src/test/java/CheckEmailUnhappyPath.java
@@ -10,12 +10,14 @@ public class CheckEmailUnhappyPath {
                 {"sohatzky@gmailcom"},
                 {"so@gmail.com"},
                 {"123456789012345678901@gmail.com"},
-                {"?????@gmail.com"},
+                {"?*^#$@gmail.com"},
+                {"sohatzky@g.com"},
                 {"sohatzky@gmaill.com"},
                 {"sohatzky@???.com"},
                 {"sohatzky@gmail.c"},
-                {"sohatzky@g.commmm"},
-                {"sohatzky@gmail.???"},
+                {"sohatzky@gmail.comytj"},
+                {"sohatzky@gmail.&?"},
+                {"sohatzky@gmail.231"},
                 {"sohatzky@gmail.COM"}};
     }
 
